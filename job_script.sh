b25d1c7ebd5a32ae774118ba7042d650a3c8ead0
James Sargant <shorinbonsai@gmail.com>
2023-01-26 10:44:16 -0500

    last run added

--- job_script.sh
+++ job_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Here you should provide the sbatch arguments to be used in all jobs in this serial farm
-#SBATCH --time=10:45:00
+#SBATCH --time=16:45:00
 #SBATCH --mem=2G
 #SBATCH --account=def-houghten
 
